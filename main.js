c6b3bfeb63498443ce22710796c533cc3039257d
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-02-27 11:54:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function addNewList() {
 }
 
 function addListItem() {
-    window.print("Hello");
     let list = document.getElementById("grocery-list");
     let itemInput = document.getElementById("text_input");
     let newItem = document.createElement("li");
