d98119ab33a78d3986e0a6dc5050769ff3f291cd
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-02-27 11:42:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function addNewList() {
 
 function addListItem() {
     let list = document.getElementById("grocery-list");
-    let itemInput = document.getElementById("text-input");
+    let itemInput = document.getElementById("text_input");
     let newItem = document.createElement("li");
     newItem.appendChild(document.createTextNode(itemInput.value));
     list.appendChild(newItem);
