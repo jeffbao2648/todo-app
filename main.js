08d61016e7a99419922e2225e4559068b060b97e
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-02-26 22:48:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
     let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("text_input");
 
 }
 
